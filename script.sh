7a31b8104e62703a38fdee63cc19b678ef1099dc
Artem Obukhov <arteam_101@mail.ru>
2022-12-07 12:46:57 +0700

    Начал проект с нуля

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 gcc src/server.c -o bin/server
-gcc src/client.c -o bin/client
+# gcc src/client.c -o bin/client
--- src/server.c
+++ src/server.c
@@ -1,64 +1,29 @@
-#include <netinet/in.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
+#include <stdio.h>
 #include <unistd.h>
-#define PORT 8080
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+int main() {
+    char message[256] = "SERVER MESSAGE";
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-int main(int argc, char const* argv[]) {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
- 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
- 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
- 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket
-         = accept(server_fd, (struct sockaddr*)&address,
-                  (socklen_t*)&addrlen))
-        < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9000);
+    server_address.sin_addr.s_addr = INADDR_ANY;
 
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
+    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+    listen(server_socket, 5);
+
+    int client_socket = accept(server_socket, NULL, NULL);
+
+    send(client_socket, message, sizeof(message), 0);
+
+    close(server_socket);
     
- 
-    // closing the connected socket
-    close(new_socket);
-    // closing the listening socket
-    shutdown(server_fd, SHUT_RDWR);
     return 0;
 }
